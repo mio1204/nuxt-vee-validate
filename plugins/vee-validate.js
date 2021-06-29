897c0384eb5ef0a854788485a0cde6f040a65c53
mio <omisiya@gmail.com>
2021-06-29 20:21:26 +0900

    更新

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
-import { ValidationProvider, ValidationObserver, extend, localize } from 'vee-validate';
+import { ValidationProvider, ValidationObserver, extend, localize, validate } from 'vee-validate';
 import * as originalRules from 'vee-validate/dist/rules';
 import ja from 'vee-validate/dist/locale/ja.json';
+
+// const customRule = {
+//   params: ['string'],
+//   message: '{_field_}が{string}と一致しません。',
+//   validate(value, {string}) {
+//     return value === string;
+//   }
+// };
+// extend('custom_rule', customRule);
+
 
 let rule;
 for (rule in originalRules) {
